32d2212abbc141d814af135bf5c301bf5d82002c
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-29 16:39:34 +0500

    Updates

---
+++ precache-manifest.e845f3bffeac36116f98b1c8afc49969.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8559b28f5a1774e97b47e6d6164d8fdd",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "7e64a844df1b81a74b60",
+    "url": "/vending-machine/static/css/main.515249c8.chunk.css"
+  },
+  {
+    "revision": "f7c2bab7844905592743",
+    "url": "/vending-machine/static/js/2.b49d374c.chunk.js"
+  },
+  {
+    "revision": "7e64a844df1b81a74b60",
+    "url": "/vending-machine/static/js/main.99f5dbaf.chunk.js"
+  },
+  {
+    "revision": "6970cc35a241b64e454a",
+    "url": "/vending-machine/static/js/runtime~main.7072e60e.js"
+  },
+  {
+    "revision": "0e21446e438c1aeb2913a4c95776436a",
+    "url": "/vending-machine/static/media/Chips.0e21446e.png"
+  },
+  {
+    "revision": "7746d7d5d94a644ffd6cdb1876f72a87",
+    "url": "/vending-machine/static/media/Soda.7746d7d5.png"
+  },
+  {
+    "revision": "369bea35cf50c57aa4cb31df6c8f4da3",
+    "url": "/vending-machine/static/media/VendingMachine.369bea35.png"
+  }
+]);
